6172eb71fcb67a8f7becd1c3954cbed7cbb9daa0
Maychell <maychellfernandes@hotmail.com>
2016-06-13 15:08:55 +0100

    fazendo upload de arquivos que nao subiram no commit anterior

--- lang/c/main.c
+++ lang/c/main.c
@@ -1,9 +1,21 @@
-#include "Gates_i.c"
 #include <stdio.h>
+#include "Gates_i.c"
+
+void gates() {
+	Gates__INITIALISATION();
+    Gates__open_gate(5);
+    Gates__close_gate(2);
+}
+
+void apartment() {
+
+}
 
 int main() {
-    printf("Hello World\n");
-    Gates__INITIALISATION();
-    Gates__open_gate(2);
+    printf("----- INICIANDO O CODIGO GERADO PELA IMPLEMENTACAO -----\n\n");
+    gates();
+    apartment();
+    printf("\n\n----- FINALIZANDO O CODIGO GERADO PELA IMPLEMENTACAO -----\n\n");
+    
     return 0;
 }
